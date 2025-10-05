bffc0f720f1c9f13ff4370fa741c9b807588cca1
CarlottaPolito <carlottapolitowd@gmail.com>
2025-10-05 18:01:03 +0200

    first commit

---
+++ src/main/java/com/example/best_of_the_year/controller/BestOfYearController.java
@@ -0,0 +1,19 @@
+package com.example.best_of_the_year.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+    @Controller
+    @RequestMapping("/")
+
+public class BestOfYearController {
+
+        @GetMapping("/best")
+        public String saluta (@RequestParam(required=false) String nome, Model model){
+            model.addAttribute("nome" , nome);
+            return "index";
+         }
+}
