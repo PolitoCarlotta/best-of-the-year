8573b041407f1d3d5e640e0cade66c5af2dcdc74
CarlottaPolito <carlottapolitowd@gmail.com>
2025-10-07 19:36:12 +0200

    step2

--- src/main/java/com/example/best_of_the_year/controller/BestOfYearController.java
+++ src/main/java/com/example/best_of_the_year/controller/BestOfYearController.java
@@ -1,5 +1,6 @@
 package com.example.best_of_the_year.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.best_of_the_year.model.Movie;
+import com.example.best_of_the_year.model.Song;
 
     @Controller
     @RequestMapping("/")
@@ -19,12 +23,46 @@ public class BestOfYearController {
             return "index";
          }
 
+
         private List<Movie> getBestMovie(){
+            List<Movie> movies = new ArrayList <> ();
 
-            List<Movie> movies = 
-         }
+            movies.add(new Movie("1", "Titanic"));
+            movies.add(new Movie("2", "Lord of the ring"));
+            movies.add(new Movie("3", "The Hobbit"));
+            movies.add(new Movie("4", "Donnie Darko"));
+            movies.add(new Movie("5", "007"));
+            movies.add(new Movie("6", "Giorgio"));
 
-        private List<Song> getBestSong(){
+            return movies;
             
          }
+
+         
+        private List<Song> getBestSong(){
+            List<Song> songs = new ArrayList <>();
+
+            songs.add(new Song("1","Fear of the dark"));
+            songs.add(new Song("2","Happy"));
+            songs.add(new Song("3","Starway to Heaven"));
+            songs.add(new Song("4","Azzurro"));
+            return songs;
+         }
+
+         @GetMapping("/movies")
+
+         public String bestMovies (Model model) {
+            List <Movie> movies = getBestMovie();
+            model.addAttribute("movies", movies);
+            return "movies";
+         }
+
+
+         @GetMapping("/songs")
+         public String bestSongs (Model model) {
+            List <Song> songs = getBestSong();
+            model.addAttribute("songs", songs);
+            return "songs";
+         }
+
 }
